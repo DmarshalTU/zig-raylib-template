39b9161b17cb4533dcf64fc0e01dfcd0a3994f99
Denis Tu <dmarshaltu@gmail.com>
2025-01-29 13:35:39 +0100

    final

--- src/main.zig
+++ src/main.zig
@@ -4,52 +4,47 @@ const rl = @cImport({
 });
 const print = @import("std").debug.print;
 
-// Embedded
-const player_texture = @embedFile("assets/cat_run.png");
+const player_run_texture_emb = @embedFile("assets/cat_run.png");
 
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
-
-    var player_position = rl.Vector2{ .x = 640, .y = 320 };
-    //const player_size = rl.Vector2{ .x = 64, .y = 64 };
-    var player_velocity = rl.Vector2{ .x = 0, .y = 0 };
-    var player_grounded: bool = false;
-    //const frames = 4;
-
-    rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    const screenWidth = 1280;
+    const screenHeight = 720;
+    rl.InitWindow(screenWidth, screenHeight, "My First Game");
     defer rl.CloseWindow(); // Close window and OpenGL context
 
-    // Assets
-    //--------------------------------------------------------------------------------------
-    const player_run_texture_mem = rl.LoadImageFromMemory(".png", player_texture, player_texture.len);
-    const p_texture = rl.LoadTextureFromImage(player_run_texture_mem);
-    const player_source = rl.Rectangle{ .x = 0, .y = 0, .width = 16, .height = 16 };
+    var player_position = rl.Vector2{ .x = 640, .y = 320 };
+    var player_velocity = rl.Vector2{ .x = 0, .y = 0 };
+    var player_grounded = false;
+    var player_flip = false;
 
-    print("PH: {}\nPW: {}\n", .{ p_texture.height, p_texture.width });
-    defer rl.UnloadTexture(p_texture);
+    const player_run_texture_mem = rl.LoadImageFromMemory(".png", player_run_texture_emb, player_run_texture_emb.len);
+    const player_run_texture = rl.LoadTextureFromImage(player_run_texture_mem);
+    const player_run_num_frames = 4;
+    var player_run_frame_timer: f32 = 0;
+    var player_run_current_frame: i32 = 0;
+    const player_run_frame_length: f32 = 0.1;
 
-    rl.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    rl.SetTargetFPS(60);
 
     // Main game loop
-    while (!rl.WindowShouldClose()) { // Detect window close button or ESC key
+    while (!rl.WindowShouldClose()) {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
         if (rl.IsKeyDown(rl.KEY_A)) {
-            player_velocity.x -= 400 * rl.GetFrameTime();
+            player_velocity.x = -400;
+            player_flip = true;
         } else if (rl.IsKeyDown(rl.KEY_D)) {
-            player_velocity.x += 400 * rl.GetFrameTime();
+            player_velocity.x = 400;
+            player_flip = false;
         } else {
             player_velocity.x = 0;
         }
 
         player_velocity.y += 2000 * rl.GetFrameTime();
 
-        if (rl.IsKeyPressed(rl.KEY_SPACE) and player_grounded) {
+        if (player_grounded and rl.IsKeyPressed(rl.KEY_SPACE)) {
             player_velocity.y = -600;
             player_grounded = false;
         }
@@ -62,25 +57,50 @@ pub fn main() anyerror!void {
             player_grounded = true;
         }
 
-        if (player_position.x > @as(f32, @floatFromInt(rl.GetScreenWidth())) - 64) {
-            player_position.x = @as(f32, @floatFromInt(rl.GetScreenWidth())) - 64;
-            player_grounded = true;
-        } else if (player_position.x <= 0) {
-            player_position.x = 0;
+        player_run_frame_timer += rl.GetFrameTime();
+
+        if (player_run_frame_timer > player_run_frame_length) {
+            player_run_current_frame += 1;
+            player_run_frame_timer = 0;
+
+            if (player_run_current_frame == player_run_num_frames) {
+                player_run_current_frame = 0;
+            }
         }
+
+        const player_run_width = @as(f32, @floatFromInt(player_run_texture.width));
+        const player_run_height = @as(f32, @floatFromInt(player_run_texture.height));
+
+        var player_source = rl.Rectangle{
+            .x = @as(f32, @floatFromInt(player_run_current_frame)) * player_run_width / @as(f32, @floatFromInt(player_run_num_frames)),
+            .y = 0,
+            .width = player_run_width / @as(f32, @floatFromInt(player_run_num_frames)),
+            .height = player_run_height,
+        };
+
+        // Flip player texture if moving left
+        if (player_flip) {
+            player_source.width = -player_source.width;
+        }
+
+        const player_dest = rl.Rectangle{
+            .x = player_position.x,
+            .y = player_position.y,
+            .width = player_run_width * 4 / @as(f32, @floatFromInt(player_run_num_frames)),
+            .height = player_run_height * 4,
+        };
 
         // Draw
         //----------------------------------------------------------------------------------
         rl.BeginDrawing();
         defer rl.EndDrawing();
 
-        rl.ClearBackground(rl.WHITE);
+        rl.ClearBackground(rl.Color{ .r = 110, .g = 184, .b = 168, .a = 255 });
 
-        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.BLACK);
-        rl.DrawTextureRec(p_texture, player_source, player_position, rl.BLACK);
-        //rl.DrawTextureV(p_texture, player_position, rl.BLACK);
-        //----------------------------------------------------------------------------------
+        rl.DrawTexturePro(player_run_texture, player_source, player_dest, .{ .x = 0, .y = 0 }, 0, rl.WHITE);
 
         rl.DrawFPS(10, 10);
     }
+
+    rl.UnloadTexture(player_run_texture);
 }
